959b77e43985b473cad3ae162e8676117df7a180
shubham-msi <skukreti.kukreti5@gmail.com>
2024-03-31 17:47:50 +0530

    Tailwind config and adding background gradient using tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
 export default function Home() {
-  return <main>Hello World!!!</main>;
+  return (
+    <main className="bg-gradient-to-b from-slate-800 to-slate-900 h-screen text-white">
+      <div id="time-period">
+        <h1>Time Period</h1>
+        <button>Ancient</button>
+        <button>Modern</button>
+        <button>Future</button>
+      </div>
+      <div>Topics</div>
+      <div></div>
+      <button>Generate story</button>
+    </main>
+  );
 }
